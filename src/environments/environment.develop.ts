b4802dbda96c5687d5526c183b6a71da4ccc34c3
Carlos Montes <krlosbergara@gmail.com>
2026-04-16 19:46:33 -0500

    update

--- src/environments/environment.develop.ts
+++ src/environments/environment.develop.ts
@@ -2,4 +2,6 @@ export const environment = {
     production: true,
     baseUrl: 'https://sso-core-dev.bigso.co',
     useV2Auth: true,
+    appId: 'sso-portal',
+    tenantId: 'tenant-default',
 };
--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -2,4 +2,6 @@ export const environment = {
     production: true,
     baseUrl: 'https://sso-core-qa.bigso.co',
     useV2Auth: true,
+    appId: 'sso-portal',
+    tenantId: 'tenant-default',
 };
