caaeec50b4f7a27790a1f11149bfd4f489bff7e8
Carlos Montes <krlosbergara@gmail.com>
2026-05-02 00:20:03 -0500

    feat: update baseUrl for development environment

--- src/environments/environment.develop.ts
+++ src/environments/environment.develop.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://sso-core.bigso.test',
+    baseUrl: 'https://sso-core-dev.bigso.co',
     useV2Auth: true,
     appId: 'sso-portal',
     tenantId: '0593fd40-96ab-4547-95c2-43a1ffb6412a',
