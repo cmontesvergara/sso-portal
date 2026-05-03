1be8db83c38d38f22804ca99a17d4daed7b06a89
Carlos Montes <krlosbergara@gmail.com>
2026-05-02 23:52:09 -0500

    chore: replace qa environment configuration with local environment settings

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,9 @@
+
+export const environment = {
+    production: false,
+    baseUrl: 'https://sso-core.bigso.test',
+    //baseUrl: 'http://sso.msoft.uno',
+    useV2Auth: true,
+    appId: 'sso-portal',
+    tenantId: '0593fd40-96ab-4547-95c2-43a1ffb6412a',
+};
--- src/environments/environment.qa.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-    baseUrl: 'https://sso-core-qa.bigso.co',
-    useV2Auth: true,
-    appId: 'sso-portal',
-    tenantId: 'tenant-default',
-};
