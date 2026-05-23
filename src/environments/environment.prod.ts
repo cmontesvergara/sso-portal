eff91df4679eddbba5f9d13ebff0c0eefca15bd4
Carlos Montes <krlosbergara@gmail.com>
2026-05-22 23:17:49 -0500

    fix: update baseUrl and appRedirectUri for production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment: any = {
   production: true,
-  baseUrl: 'https://sso-core-prod.bigso.co',
+  baseUrl: 'https://idp.bigso.org',
   useV2Auth: true,
   appId: 'sso-portal',
   tenantId: '6615ba1e-73ee-4c6c-a689-2a8359253988',
   /** Target app URL for direct (non-SDK) login. User is redirected here after authorize. */
-  appRedirectUri: 'https://sso.bigso.co/auth/callback',
+  appRedirectUri: 'https://auth.bigso.org/auth/callback',
 };
 
